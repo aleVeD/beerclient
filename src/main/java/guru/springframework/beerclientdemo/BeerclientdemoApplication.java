228ab05f231210b22151508b0230ac1d569d5113
aleVeD <aldmove@gmail.com>
2020-07-10 22:13:31 -0400

    first commit

---
+++ src/main/java/guru/springframework/beerclientdemo/BeerclientdemoApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.beerclientdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerclientdemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BeerclientdemoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/guru/springframework/beerclientdemo/BeerclientdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.beerclientdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerclientdemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
